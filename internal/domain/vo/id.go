d8e188549270b66eacd5e5b53de6c8aea931b489
Boris <glazunov2142@icloud.com>
2024-07-06 16:35:25 +0300

    Update id.go

--- internal/domain/vo/id.go
+++ internal/domain/vo/id.go
@@ -1,9 +1,7 @@
 package vo
 
 import (
-	"fmt"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"strings"
 )
 
 type ID struct {
@@ -12,22 +10,6 @@ type ID struct {
 
 func NewID(oid primitive.ObjectID) ID {
 	return ID{Value: oid}
-}
-
-func (id *ID) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf("\"%v\"", id.Value.Hex())), nil
-}
-
-func (id *ID) UnmarshalJSON(b []byte) error {
-	hex := strings.ReplaceAll(string(b), "\"", "")
-
-	oid, err := primitive.ObjectIDFromHex(hex)
-	if err != nil {
-		return err
-	}
-	id.Value = oid
-
-	return nil
 }
 
 func (id *ID) Hex() string {
